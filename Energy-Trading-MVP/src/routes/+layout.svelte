21927b3c252c3219e669987b2baa7cbbb79dcfb5
Himanshu Rawat <113584120+himanshu-rawat77@users.noreply.github.com>
2024-07-20 17:41:17 +0530

    Delete Energy-Trading-MVP directory

--- Energy-Trading-MVP/src/routes/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import Navbar from '$lib/Navbar.svelte'
-    import Footer from '$lib/Footer.svelte'
-    import "../app.css"
-    </script>
-    
-<Navbar />
-<slot />
-<Footer />
--- Energy-Trading-MVP/src/routes/+page.svelte
+++
@@ -1,2 +0,0 @@
-
-  <h1> Name of our Project </h1>

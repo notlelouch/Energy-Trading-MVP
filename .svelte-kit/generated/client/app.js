0491abfabe11aa54a54334d99060b2c9d58da2ed
Himanshu <himanshu.rawat7789@gmail.com>
2024-07-21 01:41:37 +0530

    commit

---
+++ .svelte-kit/generated/client/app.js
@@ -0,0 +1,35 @@
+export { matchers } from './matchers.js';
+
+export const nodes = [
+	() => import('./nodes/0'),
+	() => import('./nodes/1'),
+	() => import('./nodes/2'),
+	() => import('./nodes/3'),
+	() => import('./nodes/4'),
+	() => import('./nodes/5'),
+	() => import('./nodes/6'),
+	() => import('./nodes/7'),
+	() => import('./nodes/8'),
+	() => import('./nodes/9')
+];
+
+export const server_loads = [];
+
+export const dictionary = {
+		"/": [2],
+		"/buy-energy": [3],
+		"/index": [4],
+		"/list-energy": [5],
+		"/login": [6],
+		"/profile": [7],
+		"/signup": [8],
+		"/transaction-history": [9]
+	};
+
+export const hooks = {
+	handleError: (({ error }) => { console.error(error) }),
+
+	reroute: (() => {})
+};
+
+export { default as root } from '../root.svelte';
